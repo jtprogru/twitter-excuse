c509b41f601b6716268bdf40a6249ae999ff43a5
Michel Savin <jtprogru@gmail.com>
2021-04-05 21:17:33 +0300

    Fix lint error
    Signed-off-by: Michel Savin <jtprogru@gmail.com>

--- src/test_excuse.py
+++ src/test_excuse.py
@@ -3,12 +3,14 @@ import pytest
 
 from utils import get_excuse, get_hashtag
 
+
 def test_get_hashtag():
     assert isinstance(get_hashtag(), str)
 
 
 def test_get_excuse():
     assert isinstance(get_excuse(), str)
+
 
 def test_length_twitt():
     resp = get_excuse()
