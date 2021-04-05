8115ee20a67be443588f0a26ee76573abff975af
Michel Savin <jtprogru@gmail.com>
2021-04-05 21:40:36 +0300

    Refactor src/excuses/data.py src/utils.py
    Signed-off-by: Michel Savin <jtprogru@gmail.com>

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,19 @@
 # coding=utf-8
 import random
-from excuses.data import Excuses, HTag, SpecialHTag
+from excuses.data import EXCUSES, HASHTAGS, SPECIALHASHTAGS
 
 
 def get_hashtag() -> str:
     ht = ''
-    for item in SpecialHTag:
+    for item in SPECIALHASHTAGS:
         ht += ' ' + item
-    ht += ' ' + random.choice(HTag)
+    ht += ' ' + random.choice(HASHTAGS)
     return ht
 
 
 def get_excuse() -> str:
     resp = '🤖'
-    resp += random.choice(Excuses)
+    resp += random.choice(EXCUSES)
     resp += get_hashtag()
 
     return resp
