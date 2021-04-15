77c285da3afad84f26c0718bcaee46e61c614c99
Michael Savin <jtprogru@gmail.com>
2021-04-15 17:46:56 +0300

    Так правильнее
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DATE=$(LC_ALL="en_US.UTF-8"  date +"%Y-%m-%dT%H:%M")
 set -e
-SCRIPT=`basename "$0"`
+SCRIPT=$(basename "$0")
 LOG_FILE_NAME="twitterbot.log"
 
 export MYLOGDIR=/Users/jtprog/workplace/logs
